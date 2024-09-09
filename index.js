b4f1b09ea96e5b7df15c6f5c728089ec9fb2552d
Mohd Shahab <mohdshahab9520@gmail.com>
2024-09-09 15:44:01 +0530

    update cors header

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ import auth from "json-server-auth";
 const server = express();
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", '*')
-    res.header("Access-Control-Allow-Origin", '*')
-    next()
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
 })
 
 const router = jsonServer.router("./data/db.json");
-server.use("/api", router);
-server.db = router.db
 
 const middlewares = jsonServer.defaults()
+
 const rules = auth.rewriter({
     orders: 660,
     users: 600
-})
+});
 
-server.use(rules)
-server.use(auth)
-server.use(middlewares)
-server.use(router)
+server.use(rules);
+server.use(auth);
+server.use(middlewares);
+
+server.use("/api", router);
+server.db = router.db;
 
 server.listen(8000);
